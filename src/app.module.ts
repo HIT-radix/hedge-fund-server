55bb479d62224f598927eb0460b71bb1ec869798
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-07-28 22:42:24 +0500

    Configure TypeORM with MySQL and initialize environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { SnapshotsModule } from "./snapshots/snapshots.module";
 
 @Module({
-  imports: [SnapshotsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true, // Don't use in production
+    }),
+    SnapshotsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
