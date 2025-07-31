00de34f004b9d2e339a2442856b039107e9d8d82
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-08-01 02:49:33 +0500

    feat: add address constants for DAPP and resources based on environment

---
+++ src/constants/address.ts
@@ -0,0 +1,23 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const DAPP_DEFINITION_ADDRESS =
+  process.env.ENVIRONMENT === "dev"
+    ? "account_tdx_2_129zymzhffm45w5jyccyu9x0tv5xs7qs76zxzrfsd7gn76f7k5reus2"
+    : "account_rdx128er8y5hetcj98krndumys93jyerq659ug0uyk6l6ljdtd9mrcevwf";
+
+export const NODE_STAKING_COMPONENT_ADDRESS =
+  process.env.ENVIRONMENT === "dev"
+    ? "component_tdx_2_1crf2uqm3tcqqdml0tqq04wurmqrhdkca7cscsuxr45lghhvv72zgnx"
+    : "component_rdx1cqpv4nfsgfk9c2r9ymnqyksfkjsg07mfc49m9qw3dpgzrmjmsuuquv";
+
+export const NODE_STAKING_USER_BADGE_ADDRESS =
+  "resource_rdx1ntmkq3eddym9lzppx8sg2elpqwexhsppcdscwu5s7ca5u79hcaztu3";
+
+export const HIT_FOMO_NODE_LSU_ADDRESS =
+  "resource_rdx1t4d3ka2x2j35e30gh75j6hma6fccwdsft88h2v2ul4qmqshnwjmxf7";
+
+export const XRD_RESOURCE_ADDRESS =
+  process.env.ENVIRONMENT === "dev"
+    ? "resource_tdx_2_1tknxxxxxxxxxradxrdxxxxxxxxx009923554798xxxxxxxxxtfd2jc"
+    : "resource_rdx1tknxxxxxxxxxradxrdxxxxxxxxx009923554798xxxxxxxxxradxrd";
