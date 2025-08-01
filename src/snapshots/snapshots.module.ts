b9c53c6d10b727fcfdc0e57ee3ab8236f7641cf2
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-08-01 17:26:30 +0500

    refactor: update import paths in snapshots module to use absolute paths for consistency

--- src/snapshots/snapshots.module.ts
+++ src/snapshots/snapshots.module.ts
@@ -2,8 +2,8 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { SnapshotsController } from "./snapshots.controller";
 import { SnapshotsService } from "./snapshots.service";
-import { NftHolder } from "../database/entities/nft-holder.entity";
-import { LedgerState } from "../database/entities/ledger-state.entity";
+import { NftHolder } from "@/database/entities/nft-holder.entity";
+import { LedgerState } from "@/database/entities/ledger-state.entity";
 
 @Module({
   imports: [TypeOrmModule.forFeature([NftHolder, LedgerState])],
