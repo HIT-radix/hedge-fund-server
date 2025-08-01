81a48220a94918070f5859b3db50198073846d58
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-08-01 17:02:18 +0500

    feat: add TypeOrmModule imports for NftHolder and LedgerState entities in snapshots module

--- src/snapshots/snapshots.module.ts
+++ src/snapshots/snapshots.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { SnapshotsController } from "./snapshots.controller";
 import { SnapshotsService } from "./snapshots.service";
+import { NftHolder } from "../database/entities/nft-holder.entity";
+import { LedgerState } from "../database/entities/ledger-state.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([NftHolder, LedgerState])],
   controllers: [SnapshotsController],
   providers: [SnapshotsService],
 })
