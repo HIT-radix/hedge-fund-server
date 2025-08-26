021b3303fca103867c5d0bdccfa3660cfaff6233
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-08-26 16:27:14 +0500

    refactor: remove load-binary-from-url helper as it is no longer needed

--- src/wallet/helpers/load-binary-from-url.ts
+++
@@ -1,9 +0,0 @@
-import { ResultAsync } from 'neverthrow'
-import fs from 'fs/promises'
-import path from 'path'
-import { typedError } from './typed-error'
-
-const appDir = path.resolve('./')
-
-export const loadBinaryFromPath = (path: string) =>
-  ResultAsync.fromPromise(fs.readFile(`${appDir}${path}`), typedError)
