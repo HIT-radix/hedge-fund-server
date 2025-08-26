e20599f8b78d726b53c39023c21e4aeed50517a0
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-08-26 17:05:22 +0500

    refactor: standardize import syntax and improve code formatting in mnemonicToKeyPair

--- src/wallet/helpers/mnemonicToKeyPair.ts
+++ src/wallet/helpers/mnemonicToKeyPair.ts
@@ -1,20 +1,20 @@
-import bip39 from 'bip39'
-import { ok } from 'neverthrow'
-import { derivePath, getPublicKey } from 'ed25519-hd-key'
-import { secureRandom } from './secure-random'
+import * as bip39 from "bip39";
+import { ok } from "neverthrow";
+import { derivePath, getPublicKey } from "ed25519-hd-key";
+import { secureRandom } from "./secure-random";
 
-export const generateMnemonic = () => bip39.entropyToMnemonic(secureRandom(32))
+export const generateMnemonic = () => bip39.entropyToMnemonic(secureRandom(32));
 
 const mnemonicToSeed = (mnemonic: string) =>
-  ok(bip39.mnemonicToSeedSync(mnemonic).toString('hex'))
+  ok(bip39.mnemonicToSeedSync(mnemonic).toString("hex"));
 
 const deriveChildKey = (derivationPath: string, seedHex: string) =>
-  ok(derivePath(derivationPath, seedHex))
+  ok(derivePath(derivationPath, seedHex));
 
 export const mnemonicToKeyPair = (mnemonic: string, derivationPath: string) =>
   mnemonicToSeed(mnemonic)
     .andThen((seedHex: string) => deriveChildKey(derivationPath, seedHex))
     .map(({ key }) => ({
-      privateKey: key.toString('hex'),
-      publicKey: getPublicKey(key, false).toString('hex'),
-    }))
+      privateKey: key.toString("hex"),
+      publicKey: getPublicKey(key, false).toString("hex"),
+    }));
