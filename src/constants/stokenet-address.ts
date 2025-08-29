8643adc49feaf7c4c142692938f3168f0af55d89
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-08-29 23:38:39 +0500

    feat: add stokenet-address constants for validator and resource management

---
+++ src/constants/stokenet-address.ts
@@ -0,0 +1,20 @@
+export const VALIDATOR =
+  "validator_tdx_2_1svff7mkddhm9dy325f3ckx72cxqsl49ewy74667pchqfkxl7wxpa8r";
+export const VALIDATOR_BADGE =
+  "resource_tdx_2_1nfxxxxxxxxxxvdrwnrxxxxxxxxx004365253834xxxxxxxxxyerzzk:[83129f6ecd6df656922aa2638b1bcac1810fd4b9713d5d6bc1c5c09b1bfe]";
+export const LSU =
+  "resource_tdx_2_1thrg4addeue0w87wksukm86updhptw9zr7z4xlrjpecltgpfpxhxce";
+export const CLAIM_NFT =
+  "resource_tdx_2_1nfwdxtrjum4qrcd2quas5vaynwuhupwse7qszzvaq2wu5f92lr8zs8";
+export const ACCOUNT_LOCKER =
+  "locker_tdx_2_1dzr5g7fr0d2wvhvd594p09wyfx04d2zlq2zm9825kzk808g2mxwn89";
+export const FUND_MANAGER_BADGE =
+  "resource_tdx_2_1thyu69x76sulvf6agqfp5njqd3uyxxtmtrnz3v7vrzszye6yu42s77";
+export const ADMIN_BADGE =
+  "resource_tdx_2_1nfkhdgruseldvkrlgu9j3vp7uyh98t6arhfl7ugcrj6pzuraynpnfs";
+export const FUND_UNIT =
+  "resource_tdx_2_1tk84y8s6jyv43zqlpuam2d7ygg88jlmcppg5tg3nndynaejt6w8lmt";
+export const DEX_COMPONENT =
+  "component_tdx_2_1cqkgyedpwyepqy8pjrfnz0gmjjskgmrhzqyj4r440rgluppgu0zn5q";
+export const ORACLE_COMPONENT =
+  "component_tdx_2_1cz0a6nkrkqs5rks97dsck0yu9jwnpjxcf6vvlem2q0tcv9476fyj3z";
