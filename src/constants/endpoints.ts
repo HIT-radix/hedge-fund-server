9e852a193ad99301f76241724514eec4c47be095
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-10-07 19:57:17 +0500

    feat: add HIT_SERVER_URL constant for server endpoint

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -5,3 +5,5 @@ export const MORPHER_ORACLE_BACKEND_URL =
   process.env.ENVIRONMENT === "dev"
     ? "https://dev-test-radix-oracle-api.morpher.com"
     : "https://radix-oracle-api.morpher.com";
+
+export const HIT_SERVER_URL = "https://server.addix.meme";
