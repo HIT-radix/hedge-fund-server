fa5049100b751de161c50029d249e6a2fcde30c3
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-10-28 16:11:58 +0500

    feat: add buyback airdrop manifest and import ACCOUNT_LOCKER_ADDRESS

--- src/utils/manifests.ts
+++ src/utils/manifests.ts
@@ -1,4 +1,8 @@
-import { FUND_BOT_BADGE, FUND_MANAGER_COMPONENT } from "@/constants/address";
+import {
+  ACCOUNT_LOCKER_ADDRESS,
+  FUND_BOT_BADGE,
+  FUND_MANAGER_COMPONENT,
+} from "@/constants/address";
 import { typescriptWallet } from "@/wallet/config";
 
 export const get_start_unlock_owner_stake_units_manifest = async (
@@ -114,4 +118,49 @@ export const get_fund_units_distribution_manifest = async (
       )
       ${moreLeft}  
     ;`;
+};
+
+export const get_buyback_airdrop_manifest = async (
+  tokenAddress: string,
+  airdropData: { address: string; amount: string }[]
+) => {
+  const addressResult = await typescriptWallet.getAccountAddress();
+  if (addressResult.isErr()) {
+    throw new Error(`Failed to get account address: ${addressResult.error}`);
+  }
+
+  const accountAddress = addressResult.value;
+  return `
+    CALL_METHOD
+        Address("${accountAddress}")
+        "create_proof_of_amount"
+        Address("${FUND_BOT_BADGE}")
+        Decimal("1")
+    ;
+    TAKE_ALL_FROM_WORKTOP
+        Address("${tokenAddress}")
+        Bucket("bucket1")
+    ;
+    CALL_METHOD
+        Address("${ACCOUNT_LOCKER_ADDRESS}")
+        "airdrop"
+        Map<Address, Enum>(
+            ${airdropData
+              .map(
+                (item) => `Address("${item.address}") => Enum<0u8>(
+                Decimal("${item.amount}")
+            )`
+              )
+              .join(",\n            ")}
+        )
+        Bucket("bucket1")
+        true
+    ;
+    CALL_METHOD
+        Address("${accountAddress}")
+        "try_deposit_batch_or_abort"
+        Expression("ENTIRE_WORKTOP")
+        Enum<0u8>()
+    ;
+  `;
 };
