da9327075425817d7e1de41140469aadd8b3034a
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2026-01-10 15:11:05 +0500

    feat: add chunkArray utility function for array manipulation

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -117,3 +117,11 @@ const attemptToSendTxOnChain = async (
     };
   }
 };
+
+export function chunkArray<T>(items: T[], chunkSize: number): T[][] {
+  const chunks: T[][] = [];
+  for (let i = 0; i < items.length; i += chunkSize) {
+    chunks.push(items.slice(i, i + chunkSize));
+  }
+  return chunks;
+}
