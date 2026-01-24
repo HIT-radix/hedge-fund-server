e9a7576a8816d1d68cee15f143690d04363d9bf6
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2026-01-25 02:09:21 +0500

    feat: add Protocol entity for database schema

---
+++ src/database/entities/protocol-metadata.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity("protocols")
+export class Protocol {
+  @PrimaryColumn({ type: "varchar", length: 255 })
+  name: string;
+
+  @Column({ type: "varchar", length: 100 })
+  platform_name: string;
+
+  @Column({ type: "varchar", length: 255 })
+  logo_image: string;
+
+  @Column({ type: "varchar", length: 255, nullable: true })
+  apyid: string | null;
+
+  @Column({ type: "varchar", length: 255, nullable: true })
+  description: string | null;
+}
